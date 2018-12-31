aee7e0f8415a3cbad00a69f64962b69c26465498
Sarah Flynn <sarah.flynn@icloud.com>
2018-12-30 21:24:29 -0800

    connects to mlab database

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('./util/connect')();
 
 const app = express();
 
 app.get('/', (req, res) => res.send('hello'));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.listen(port, () => console.log(`Server running on ${port}`));
---
+++ util/connect.js
@@ -0,0 +1,49 @@
+/* eslint-disable  no-console */
+require('dotenv').config();
+const { parse } = require('url');
+const mongoose = require('mongoose');
+
+const log = (event, dbUrl) => {
+  return () => {
+    console.log(`${event.toUpperCase()}: connection to ${dbUrl}`);
+  };
+};
+
+const redactURLAuth = url => {
+  const parsedUrl = parse(url);
+  const redactedAuth = parsedUrl.auth ? '***:***@' : '';
+
+  return `${parsedUrl.protocol}//${redactedAuth}${parsedUrl.hostname}:${
+    parsedUrl.port
+  }${parsedUrl.path}`;
+};
+
+module.exports = (dbUrl = process.env.MONGODB_URI) => {
+  mongoose.connect(
+    dbUrl,
+    {
+      auth: {
+        user: process.env.USERNAME,
+        password: process.env.PASSWORD
+      },
+      useNewUrlParser: true
+    }
+  );
+
+  const redactedUrl = redactURLAuth(dbUrl);
+
+  mongoose.connection.on('error', log('error', redactedUrl));
+
+  mongoose.connection.on('open', log('open', redactedUrl));
+
+  mongoose.connection.on('close', log('close', redactedUrl));
+
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+      console.log(
+        'Mongoose default connection disconnected through app termination'
+      );
+      process.exit(0);
+    });
+  });
+};
